1486151798032fe1183d95170bb91e3c88736dd6
Dmitry Tarasov <brahmanchik@gmail.com>
2025-11-16 00:09:01 +0300

    добавил в индекс файл с шаблоном и файл index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -12,6 +13,15 @@ $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 // Define app routes
+$app->get('/', function (Request $request, Response $response) {
+    $renderer = new PhpRenderer(__DIR__ . '/../templates');
+    $readmeContent = 'Для того чтобы увидеть роутинг в http запросe в конце url адреса добавтье это - /hello/user';
+    $viewData = [
+        'key1' => $readmeContent
+    ];
+    return $renderer->render($response, 'index.phtml', $viewData);
+});
+
 $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
