4b1e0f19bbadf428e1aeba66cb7d8b457a7419f7
Dmitry Tarasov <brahmanchik@gmail.com>
2025-11-24 21:47:36 +0300

    создал БД для разработки и развернутого приложения, добавил роутинг обработчик, который добавляет введенную информацию в базу данных, и начал делать валидацию url

--- public/index.php
+++ public/index.php
@@ -1,16 +1,26 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 
-require __DIR__ . '/../vendor/autoload.php';
+use Illuminate\Validation\Factory;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
+use Illuminate\Support\MessageBag;
 
 $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$dsn = 'pgsql:host=127.0.0.1;port=5432;dbname=hexlet_db_dev;';
+$user = 'a1111';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
 
 // Define app routes
 $app->get('/', function (Request $request, Response $response) {
@@ -22,10 +32,44 @@ $app->get('/', function (Request $request, Response $response) {
     return $renderer->render($response, 'index.phtml', $viewData);
 });
 
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
+$app->post('/urls', function (Request $request, Response $response) use ($dbh) {
+    $data = $request->getParsedBody();
+    $urlName = $data['url']['name'] ?? null;
+    //ниже будет валидация
+    // Создаём Translator (заглушка)
+    $translator = new Translator(new ArrayLoader(), 'en');
+
+// Создаём Factory
+    $factory = new Factory($translator);
+
+// Данные для проверки
+    $data = [
+        'url' => $urlName
+    ];
+
+// Правила валидации
+    $rules = [
+        'url' => 'required|url'
+    ];
+
+// Создаём Validator
+    $validator = $factory->make($data, $rules);
+
+// Проверяем
+    if ($validator->fails()) {
+        dd("Неверный URL!\n");
+        print_r($validator->errors()->all());
+    } else {
+        dd("URL верный!\n");
+    }
+    //выше валидация
+    $stmt = $dbh->prepare("INSERT INTO urls (name) VALUES (:name)");
+    $stmt->bindValue(':name', $urlName, PDO::PARAM_STR);
+    $stmt->execute();
+
+    return $response
+        ->withHeader('Location', '/')
+        ->withStatus(302);
 });
 
 // Run app
